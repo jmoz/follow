125cdfce078e028d5df4e351badde411bdc1458c
James Morris <james@jmoz.co.uk>
2014-05-29 14:18:45 +0100

    changed start to use workon

--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 pgrep redis-server || redis-server ~/Dropbox/configs/redis.conf
-. ./venv/bin/activate && celery worker --autoreload --loglevel=info -A tasks -B
+source /usr/local/bin/virtualenvwrapper.sh
+workon follow && celery worker --autoreload --loglevel=info -A tasks -B
