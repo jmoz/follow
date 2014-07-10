84bb226d0411a9a58b59181b43a116aff97b60a6
James Morris <james@jmoz.co.uk>
2014-07-10 22:27:35 +0100

    test method

--- test_twitter.py
+++ test_twitter.py
@@ -4,10 +4,11 @@ from dateutil import parser
 from model import Twitter, is_older_than_days
 
 
-# test date comparison
-date_to_compare = parser.parse(u'Thu Nov 23 11:09:20 +0000 2013').replace(tzinfo=None)
-date_22 = u'Thu Nov 22 11:09:20 +0000 2013'
-date_20 = u'Thu Nov 20 11:09:20 +0000 2013'
+def test_date_comparison():
+    # test date comparison
+    date_to_compare = parser.parse(u'Thu Nov 23 11:09:20 +0000 2013').replace(tzinfo=None)
+    date_22 = u'Thu Nov 22 11:09:20 +0000 2013'
+    date_20 = u'Thu Nov 20 11:09:20 +0000 2013'
 
-assert_false(is_older_than_days(date_22, 2, date_to_compare))
-assert_true(is_older_than_days(date_20, 2, date_to_compare))
+    assert_false(is_older_than_days(date_22, 2, date_to_compare))
+    assert_true(is_older_than_days(date_20, 2, date_to_compare))
